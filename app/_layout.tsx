462ba773e8f68291222a7f73e3612b3db029519a
AbuBokorprog <abubokor1066@gmail.com>
2025-07-03 17:23:36 +0600

    Enhance layout styling with custom header and tab bar colors

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <>
+      <StatusBar style="light" />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" options={{}} />
-    </Stack>
+    </>
   );
 }
