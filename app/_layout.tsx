f49c4aff87d85e12bd0b51553d28e73324d4c3f8
AbuBokorprog <abubokor1066@gmail.com>
2025-07-08 11:12:24 +0600

    Add Button and ImageViewer components
    - Implemented a Button component with primary theme support and icon integration.
    - Created an ImageViewer component for displaying images with rounded corners.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,10 +3,10 @@ import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
-    <>
+    <Stack>
       <StatusBar style="light" />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" options={{}} />
-    </>
+    </Stack>
   );
 }
